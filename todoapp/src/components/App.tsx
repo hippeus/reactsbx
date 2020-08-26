7549484b124c728becb6316db3320f8cd7772957
hippeus <plachta.maciej@gmail.com>
2020-08-26 21:17:33 +1000

    feat(todoapp): make react-reduct, react-thunk work with typescript

--- todoapp/src/components/App.tsx
+++ todoapp/src/components/App.tsx
@@ -1,21 +1,49 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
+import { connect, ConnectedProps } from "react-redux";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers";
 
-interface AppProps {
-  todos: Todo[];
-  fetchTodos(): any;
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+const mapDispatchToProps = { fetchTodos, deleteTodo };
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type AppProps = ConnectedProps<typeof connector>;
+
+interface AppState {
+  fetching: boolean;
 }
 
-class _App extends React.Component<AppProps> {
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { fetching: false };
+  }
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
   };
 
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
     });
   }
 
@@ -23,14 +51,11 @@ class _App extends React.Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? "LOADING" : null}
         {this.renderList()}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
-  return { todos };
-};
-
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connector(_App);
