e02867ad5a70c3f232bfc0a2f0538e8090598919
hippeus <plachta.maciej@gmail.com>
2020-08-28 19:16:11 +1000

    feat(tally): reusable solution using composition

---
+++ tally/src/MatchReader.ts
@@ -0,0 +1,33 @@
+import { MatchResult } from "./MatchResult";
+import { dateStringToDate } from "./utils";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export interface DataReader {
+  data: string[][];
+  read(): void;
+}
+
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, // type assertion
+          row[6],
+        ];
+      }
+    );
+  }
+}
--- tally/src/index.ts
+++ tally/src/index.ts
@@ -1,12 +1,14 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader("football.csv");
-reader.read();
+const reader = new MatchReader(new CsvFileReader("football.csv"));
+
+reader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of reader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
